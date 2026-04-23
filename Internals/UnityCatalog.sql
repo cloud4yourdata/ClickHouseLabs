e83814b176aa8f5f152062370533b4588f657de8
Tomasz Krawczyk <tomasz.k.krawczyk@gmail.com>
2026-04-23 18:19:41 +0200

    ClickhouseLabs

--- Internals/UnityCatalog.sql
+++ Internals/UnityCatalog.sql
@@ -2,7 +2,7 @@ SET allow_experimental_database_unity_catalog = 1;
 --https://dbc-b9b4b97b-d0dc.cloud.databricks.com/
 CREATE DATABASE IF NOT EXISTS `sqldaylite2025-dbr`
 ENGINE = DataLakeCatalog('https://dbc-b9b4b97b-d0dc.cloud.databricks.com/api/2.1/unity-catalog')
-SETTINGS warehouse = 'labs', catalog_credential = 'dapi6c63267a240111d48e5f8346b03f1401', catalog_type = 'unity'
+SETTINGS warehouse = 'labs', catalog_credential = '*****', catalog_type = 'unity'
 
 --
 172.22.64.1 -IP
